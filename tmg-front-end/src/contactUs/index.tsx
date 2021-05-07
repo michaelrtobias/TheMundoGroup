1d757f3688735d72a7360ec2d5f69cadca6ebf92
Michael Tobias <michaelremitobias@gmail.com>
2021-05-06 18:14:44 -0700

    form input elements are updating state

--- tmg-front-end/src/contactUs/index.tsx
+++ tmg-front-end/src/contactUs/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
@@ -17,7 +17,31 @@ const BorderWrapper = styled.div`
   padding: 10px;
 `;
 
-export default function ContactUs() {
+interface Entry {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  type: string;
+  make: string;
+  model: string;
+  description: string;
+  image_URL: string;
+}
+
+const ContactUs: React.FC = () => {
+  // const [first_name, setFirstName] = useState("brian");
+  // const [last_name, setLastName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [phone, setPhone] = useState("");
+  // const [type, setType] = useState("");
+  // const [make, setMake] = useState("");
+  // const [model, setModel] = useState("");
+  // const [description, setDescription] = useState("");
+  // const [image_URL, setImageURL] = useState("");
+
+  const [wishlistEntry, setWishlistEnty] = useState<Partial<Entry>>({});
+
   return (
     <BorderWrapper>
       <FormWrapper>
@@ -28,15 +52,29 @@ export default function ContactUs() {
             <Form.Group as={Col} controlId="formFirstName">
               <Form.Label>First Name</Form.Label>
               <Form.Control
-                type="string"
+                type="text"
+                value={wishlistEntry.first_name || ""}
                 placeholder="Enter First Name"
+                onChange={(e) =>
+                  setWishlistEnty({
+                    ...wishlistEntry,
+                    first_name: e.target.value,
+                  })
+                }
               ></Form.Control>
             </Form.Group>
             <Form.Group as={Col} controlId="formLastName">
               <Form.Label>Last Name</Form.Label>
               <Form.Control
-                type="string"
+                type="text"
                 placeholder="Enter Last Name"
+                value={wishlistEntry.last_name || ""}
+                onChange={(e) =>
+                  setWishlistEnty({
+                    ...wishlistEntry,
+                    last_name: e.target.value,
+                  })
+                }
               ></Form.Control>
             </Form.Group>
           </Form.Row>
@@ -46,6 +84,13 @@ export default function ContactUs() {
               <Form.Control
                 type="email"
                 placeholder="Enter Email"
+                value={wishlistEntry.email || ""}
+                onChange={(e) =>
+                  setWishlistEnty({
+                    ...wishlistEntry,
+                    email: e.target.value,
+                  })
+                }
               ></Form.Control>
             </Form.Group>
             <Form.Group as={Col} controlId="formPhone">
@@ -53,26 +98,67 @@ export default function ContactUs() {
               <Form.Control
                 type="phone"
                 placeholder="Enter Phone Number"
+                value={wishlistEntry.phone || ""}
+                onChange={(e) =>
+                  setWishlistEnty({
+                    ...wishlistEntry,
+                    phone: e.target.value,
+                  })
+                }
               ></Form.Control>
             </Form.Group>
           </Form.Row>
           <Form.Row>
             <Form.Group>
-              <Form.Label>What tye of goods are you looking for?</Form.Label>
-              <Form.Control as="select" defaultValue="Type..."></Form.Control>
+              <Form.Label>What type of goods are you looking for?</Form.Label>
+              <Form.Control
+                as="select"
+                defaultValue="Type..."
+                value={wishlistEntry.type || ""}
+                onChange={(e) =>
+                  setWishlistEnty({
+                    ...wishlistEntry,
+                    type: e.target.value,
+                  })
+                }
+              >
+                <option>Type...</option>
+                <option>Watch</option>
+                <option>Parts & Accessories</option>
+                <option>Diamonds</option>
+                <option>Bracelets</option>
+                <option>Gold Link</option>
+                <option>Belts & Buckels</option>
+                <option>Leather Goods</option>
+                <option>Other</option>
+              </Form.Control>
             </Form.Group>
             <Form.Group as={Col} controlId="formMake">
               <Form.Label>Make or Brand</Form.Label>
               <Form.Control
-                type="string"
+                type="text"
                 placeholder="Enter Make"
+                value={wishlistEntry.make || ""}
+                onChange={(e) =>
+                  setWishlistEnty({
+                    ...wishlistEntry,
+                    make: e.target.value,
+                  })
+                }
               ></Form.Control>
             </Form.Group>
             <Form.Group as={Col} controlId="formModel">
               <Form.Label>Model</Form.Label>
               <Form.Control
-                type="string"
+                type="text"
                 placeholder="Enter Model"
+                value={wishlistEntry.model || ""}
+                onChange={(e) =>
+                  setWishlistEnty({
+                    ...wishlistEntry,
+                    model: e.target.value,
+                  })
+                }
               ></Form.Control>
             </Form.Group>
           </Form.Row>
@@ -83,16 +169,22 @@ export default function ContactUs() {
                 Bezel, etc...
               </Form.Label>
               <Form.Control
-                size="lg"
                 type="text"
                 placeholder="Enter Description"
+                value={wishlistEntry.description || ""}
+                onChange={(e) =>
+                  setWishlistEnty({
+                    ...wishlistEntry,
+                    description: e.target.value,
+                  })
+                }
               />
             </Form.Group>
           </Form.Row>
           <Form.Row></Form.Row>
           <Container>
             <Form.Row>
-              <Col md={3}>
+              <Col md={4}>
                 <Form.Group as={Col} controlId="formMake">
                   <Form.File
                     id="productFormImageUplaod"
@@ -117,4 +209,6 @@ export default function ContactUs() {
       </FormWrapper>
     </BorderWrapper>
   );
-}
+};
+
+export default ContactUs;
